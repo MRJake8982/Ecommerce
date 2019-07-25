9cb8cd62ac348adf35118181489c713cd4dcee92
MrJakesss <jaque8982@gmail.com>
2019-07-24 21:24:22 -0400

    24-07-2019

--- resources/views/articulos/detalle.blade.php
+++ resources/views/articulos/detalle.blade.php
@@ -1,13 +1,14 @@
 @extends('plantilla-adm')
 
 @section('seccion-adm')
-<
-<h1>id:{{$articulo->id}}</h1>
+<div class="container">
+<h1 >id:{{$articulo->id}}</h1>
 
 <h1>Nombre:{{$articulo->nombre}}</h1>
 
 <h1>plataforma:{{$articulo->Plataforma}}</h1>
+
 <h1>Precio:{{$articulo->precio}}</h1>
 
-
+</div>
 @endsection
